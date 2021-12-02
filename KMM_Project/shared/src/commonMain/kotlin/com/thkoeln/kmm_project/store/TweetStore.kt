8c6075c80a10c16d1d0c97a1dfef695e25845679
Nico Thometzki (inf1695) <nico.thometzki@smail.th-koeln.de>
2021-12-02 11:48:36 +0100

    New State management; Undone

--- KMM_Project/shared/src/commonMain/kotlin/com/thkoeln/kmm_project/store/TweetStore.kt
+++ KMM_Project/shared/src/commonMain/kotlin/com/thkoeln/kmm_project/store/TweetStore.kt
@@ -5,65 +5,47 @@ import com.arkivanov.mvikotlin.core.store.Reducer
 import com.arkivanov.mvikotlin.core.store.Store
 import com.arkivanov.mvikotlin.core.store.StoreFactory
 import com.arkivanov.mvikotlin.core.store.create
+import com.arkivanov.mvikotlin.core.store.*
 import com.arkivanov.mvikotlin.extensions.reaktive.ReaktiveExecutor
-
+import com.thkoeln.kmm_project.Tweet
+//import android.content.Intent
 import com.thkoeln.kmm_project.store.TweetStore.Intent
+import com.thkoeln.kmm_project.store.TweetStore.State
+
+
 
 internal interface TweetStore : Store<Intent, State, Nothing> {
 
     sealed class Intent {
-        object Increment : Intent()
-        object Decrement : Intent()
-        data class Sum(val n: Int) : Intent()
+        data class AddTweet(val tweet: Tweet) : Intent()
     }
 
     data class State(
-        val value: Long = 0L
+        val value: Array<Tweet> = arrayOf()
     )
 }
 
 internal class TweetStoreFactory(private val storeFactory: StoreFactory) {
 
-
     private sealed class Result {
-        class Value(val value: Long) : Result()
+        class Tweet(val tweet: Tweet) : Result()
     }
 
     fun create(): TweetStore =
         object : TweetStore, Store<Intent, State, Nothing> by storeFactory.create(
             name = "CounterStore",
-            initialState = State.INITIALIZED,
-            reducer = ReducerImpl,
-
-            ) {
+            initialState = State(),
+            reducer = ReducerImpl
+        ) {
         }
-
-
-
 
     private object ReducerImpl : Reducer<State, Result> {
         override fun State.reduce(result: Result): State =
             when (result) {
-                is Result.Value -> copy(value = result.value)
+                is Result.Tweet -> copy(value = arrayOf(result.tweet))
             }
-    }
-
-
-
-    private class ExecutorImpl : ReaktiveExecutor<Intent, Nothing, State, Result, Nothing>() {
-        override fun executeIntent(intent: Intent, getState: () -> State) =
-            when (intent) {
-                is Intent.Increment -> dispatch(Result.Value(getState().value + 1))
-                is Intent.Decrement -> dispatch(Result.Value(getState().value - 1))
-                is Intent.Sum -> sum(intent.n)
-            }
-
-        private fun sum(n: Int) {
-            singleFromFunction { (1L..n.toLong()).sum() }
-                .subscribeOn(computationScheduler)
-                .map(Result::Value)
-                .observeOn(mainScheduler)
-                .subscribeScoped(onSuccess = ::dispatch)
-        }
     }
 }
+
+//fun copy(name: String = this.name, age: Int = this.age) = User(value)
+//fun copy(value: Long) = value
